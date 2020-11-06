3ad3015343753ece45edf7e30a909bff6c468716
flis_ann <ann.flismark@gmail.com>
2020-11-06 20:32:02 +0100

    CSS changes

--- src/About.jsx
+++ src/About.jsx
@@ -5,7 +5,7 @@ const About = () => {
   return (
     <Container>
       <h1 id="about-header">About Me</h1>
-      <img src="./images/ann.png"></img>
+      <img src="./images/ann.png" alt="ann"></img>
         <p>
           I am a positive person who enjoys working with great people. Creativity, whiteboards, pens and colours make me happy and inspired. 
           One of my passions is working with processes and trying new things to improve the way we work and continuously learn how to be better.
