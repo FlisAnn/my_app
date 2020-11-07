563ef7e0eb1006f59b912ec26a375df145a3c199
flis_ann <ann.flismark@gmail.com>
2020-11-07 12:28:10 +0100

    Testing some Semantic UI code

--- src/Resume.jsx
+++ src/Resume.jsx
@@ -5,6 +5,17 @@ const Resume = () => {
   return (
     <Container>
         <h1 id="resume-header">My Resume</h1>
+
+        <h2>Experience</h2>
+        <div class="ui list">
+  <div class="item">
+    <img class="ui avatar image" src="/images/avatar2/small/rachel.png">
+    <div class="content">
+      <a class="header">Rachel</a>
+      <div class="description">Last seen watching <a><b>Arrested Development</b></a> just now.</div>
+    </div>
+  </div>
+
     </Container>
   );
 };
