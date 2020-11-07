631f2b732068797a0321d17f342bec8d5baad0e7
flis_ann <ann.flismark@gmail.com>
2020-11-07 22:11:54 +0100

    rearranging p tags

--- src/Hello.jsx
+++ src/Hello.jsx
@@ -11,9 +11,9 @@ const Hello = () => {
            <p> 
             Welcome, I'm glad you dropped by. 
             Here you can find fun facts about me, see some of the projects I've been working on and also find my CV. 
-           </p>         
+                   
           <img src="./images/hello.png" alt="hello"></img>
-          <p>
+   
             So have a look around and if you want to get in contact go to <a href="https://www.linkedin.com/in/annflismark/" target="_blank" rel="noreferrer">my linkedin page</a> and connect with me! 
           </p>
           
