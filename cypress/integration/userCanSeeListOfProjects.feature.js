9c8ee2e0dfcbd15df5957b0416573ac99103badc
flis_ann <ann.flismark@gmail.com>
2020-11-07 12:06:31 +0100

    REverting to no linl in json

--- cypress/integration/userCanSeeListOfProjects.feature.js
+++ cypress/integration/userCanSeeListOfProjects.feature.js
@@ -9,7 +9,6 @@ describe("User can see list of projects", () => {
       cy.get(".image").should("exist");
       cy.get(".header").should("contain", "Address Book");
       cy.get(".description").should("contain", "In this project I used advanced Javascript concepts and ES6+ Features, persisting data using LocalStorage, testing with Cypress"); 
-      cy.get(".link").should("contain", "https://github.com/FlisAnn/atm_challenge_2020"); 
     });
   });
 
