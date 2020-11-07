8479df206a535de0278ed1c67aa4385abff01c18
flis_ann <ann.flismark@gmail.com>
2020-11-07 21:53:31 +0100

    Adding welcome image to hello

--- src/Hello.jsx
+++ src/Hello.jsx
@@ -6,8 +6,10 @@ const Hello = () => {
       <Container>
           <h1 id="hello">Hello World</h1>
           <p>
-            It's me, Ann. Welcome, I'm glad you dropped by. Here you can find fun facts about, see some of the projects I've been working on and also find my CV. So have a look around and if you want to get in contact go to <a href="https://www.linkedin.com/in/annflismark/">my linkedin page</a> and connect with me! 
+            It's me, Ann. Welcome, I'm glad you dropped by. 
+            Here you can find fun facts about me, see some of the projects I've been working on and also find my CV. So have a look around and if you want to get in contact go to <a href="https://www.linkedin.com/in/annflismark/" target="_blank">my linkedin page</a> and connect with me! 
           </p>
+          <img src="./images/hello.png" alt="hello"></img>
           
            
           
