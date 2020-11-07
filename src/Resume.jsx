b3039c37e223ad53fcf7ffb4846a35cd0c9d052e
flis_ann <ann.flismark@gmail.com>
2020-11-07 14:51:48 +0100

    Removing WorkCard and adding work to education.json instead

--- src/Resume.jsx
+++ src/Resume.jsx
@@ -2,7 +2,6 @@ import React, { Component } from "react";
 import { Container, Grid } from "semantic-ui-react";
 import axios from "axios";
 import EducationCard from './EducationCard';
-import WorkCard from './WorkCard';
 
 class Education extends Component {
   state = {
@@ -36,36 +35,3 @@ class Education extends Component {
   }  
 
 export default Education;
-
-class Work extends Component {
-  state = {
-    work: [],
-  };
-
-  componentDidMount() {
-    axios({url:"../data/work.json", method: "get", headers: { 'Content-Type': 'application/json' }}).then((response) => {
-      this.setState({ work: response.data });
-    });
-  }
-
-  render() {
-    const { work } = this.state;
-
-    let workList = work.map((work) => {
-      return (
-        <div id={`work-${work.id}`} key={work.id}>
-          <WorkCard work={work} />
-        </div>
-      );
-    });
-
-    return (
-        <Container>
-          <h1 id="resume-header">My resume</h1>
-          <Grid>{workList}</Grid>
-        </Container>
-      );
-    }
-  }  
-
-export default Work;
