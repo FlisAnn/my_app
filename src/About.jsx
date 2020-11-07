e41bb3cf77b0493c5e2b1a1c378dfb6535cf3e50
flis_ann <ann.flismark@gmail.com>
2020-11-07 20:04:35 +0100

    trying out sticky footer

--- src/About.jsx
+++ src/About.jsx
@@ -10,8 +10,11 @@ const About = () => {
           I am a positive person who enjoys working with great people. Creativity, whiteboards, pens and colours make me happy and inspired. 
           One of my passions is working with processes and trying new things to improve the way we work and continuously learn how to be better.
         </p>
-        Besides being really into becoming a web developer I have two cats called Pip and Mio. And a son that is 2 years old.
-          <img src="./images/pipochmio.png" alt="cats"></img><img src="./images/kotten.png" alt="kid"></img>
+        <p>
+          <img src="./images/pipochmio.png" alt="cats"></img>
+          Besides being really into becoming a web developer I have two cats called Pip and Mio. And a son that is 2 years old.
+          <img src="./images/kotten.png" alt="kid"></img>
+        </p>
         <p>
           I have long experience from working in and with agile processes and I have tried out different roles such as Test Manager, Scrum Master and tester. 
           I am driven by solving problems and enjoy working in projects with awesome colleagues, variation and new challenges.
