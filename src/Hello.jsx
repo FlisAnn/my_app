44001741cb6f4356bdbae5dc6f19d3ee17ccacdd
flis_ann <ann.flismark@gmail.com>
2020-11-07 21:58:28 +0100

    Adding rel=noreferrer

--- src/Hello.jsx
+++ src/Hello.jsx
@@ -10,7 +10,7 @@ const Hello = () => {
            </p>
            <p> 
             Welcome, I'm glad you dropped by. 
-            Here you can find fun facts about me, see some of the projects I've been working on and also find my CV. So have a look around and if you want to get in contact go to <a href="https://www.linkedin.com/in/annflismark/" target="_blank">my linkedin page</a> and connect with me! 
+            Here you can find fun facts about me, see some of the projects I've been working on and also find my CV. So have a look around and if you want to get in contact go to <a href="https://www.linkedin.com/in/annflismark/" target="_blank" rel="noreferrer">my linkedin page</a> and connect with me! 
           </p>
           <img src="./images/hello.png" alt="hello"></img>
           
