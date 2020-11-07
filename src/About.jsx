2acffa33b6ee4fe0888e5b5821f959c43f145659
flis_ann <ann.flismark@gmail.com>
2020-11-07 20:23:21 +0100

    trying out sticky footer pt 4

--- src/About.jsx
+++ src/About.jsx
@@ -5,26 +5,23 @@ const About = () => {
   return (
     <Container>
       <h1 id="about-header">About Me</h1>
-      <img src="./images/ann.png" alt="ann"></img>
-        <p>
-          I am a positive person who enjoys working with great people. Creativity, whiteboards, pens and colours make me happy and inspired. 
-          One of my passions is working with processes and trying new things to improve the way we work and continuously learn how to be better.
-        </p>
-        <p>
-          <img src="./images/pipochmio.png" alt="cats"></img>
-          Besides being really into becoming a web developer I have two cats called Pip and Mio. And a son that is 2 years old.
-          <img src="./images/kotten.png" alt="kid"></img>
-        </p>
-        <p>
-          I have long experience from working in and with agile processes and I have tried out different roles such as Test Manager, Scrum Master and tester. 
-          I am driven by solving problems and enjoy working in projects with awesome colleagues, variation and new challenges.
-        </p>
-        <p>
-          
-        </p>
-        <p>     
-          To come in contact with me, please send me an email at 
-        </p>
+        <img src="./images/ann.png" alt="ann"></img>
+          <p>
+            I am a positive person who enjoys working with great people. Creativity, whiteboards, pens and colours make me happy and inspired. 
+            One of my passions is working with processes and trying new things to improve the way we work and continuously learn how to be better.
+          </p>
+          <p>
+            <img src="./images/pipochmio.png" alt="cats"></img>
+            Besides being really into becoming a web developer I have two cats called Pip and Mio. And a son that is 2 years old.
+            <img src="./images/kotten.png" alt="kid"></img>
+          </p>
+          <p>
+            I have long experience from working in and with agile processes and I have tried out different roles such as Test Manager, Scrum Master and tester. 
+            I am driven by solving problems and enjoy working in projects with awesome colleagues, variation and new challenges.
+          </p>
+          <p>     
+            To come in contact with me, please send me an email at 
+          </p>
 
     </Container>
   );
